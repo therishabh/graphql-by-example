67676b3b48345cf21ea1aa17996f1b91b8edc768
Rishabh Agrawal <r.agrawal@dyninno.in>
2025-08-15 00:59:45 +0530

    Implement job query and update schema for Job and Company types

--- reducers.js
+++ reducers.js
@@ -1,5 +1,27 @@
+import { getCompany } from "./db/companies.js";
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    greeting: () => "Hello There Rishabh Agrawal!",
+    job: async () => {
+      const jobs = await getJobs();
+      console.log(jobs);
+      return jobs;
+    },
+  },
+
+  Job: {
+    date: (job) => {
+      return toIsoDate(job.createdAt);
+    },
+    company: async (job) => {
+      const company = await getCompany(job.companyId);
+      return company;
+    },
   },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+  v;
+}
